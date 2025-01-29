3f06737e581438e35b649b2255e83e729a078c42
Sunny Sls <sunny@backbase.com>
2025-01-29 15:29:35 -0800

    SET UP

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+const msg = "This is in Tailwind!!";
+</script>
+
+<template>
+  <div
+    class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+    role="alert"
+  >
+    <strong class="font-bold">{{ msg }}</strong>
+    <span class="block sm:inline">
+      Something seriously is about to happen.</span
+    >
+  </div>
+  
+
+  
+
+  
+
+
+  <div class="m-8">
+    <router-view></router-view>
+  </div>
+
+</template>
+
+<style scoped></style>
---
+++ src/components/BindDirective.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+const colorVal = ref('lightGreen');
+</script>
+<template>
+    <h2> Bind Directive </h2>
+    <h2>Example v-bind Directive</h2>
+  <p>The v-bind directive connects the style attribute of the DIV element to the 'colorVal' data property.</p>
+  <div v-bind:style="{ backgroundColor: colorVal }">DIV element</div>
+  <p>Use the input type="color" box below to change the color.</p>
+  <p><input type="color" v-model="colorVal"> <pre>colorVal: '{{ colorVal }}'</pre></p>
+</template>
