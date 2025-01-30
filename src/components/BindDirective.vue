197ae7366fef5a34b1c06379fb7fdbe88ccaa874
Sunny Sls <sunny@backbase.com>
2025-01-29 16:35:39 -0800

    Bind Directive: Added bind directive example

--- src/components/BindDirective.vue
+++ src/components/BindDirective.vue
@@ -3,10 +3,16 @@ import {ref} from 'vue'
 const colorVal = ref('lightGreen');
 </script>
 <template>
-    <h2> Bind Directive </h2>
+  <div class="max-w-screen-xl block  p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <span>
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> Bind Directive </h5>
+    </span>
     <h2>Example v-bind Directive</h2>
   <p>The v-bind directive connects the style attribute of the DIV element to the 'colorVal' data property.</p>
   <div v-bind:style="{ backgroundColor: colorVal }">DIV element</div>
   <p>Use the input type="color" box below to change the color.</p>
   <p><input type="color" v-model="colorVal"> <pre>colorVal: '{{ colorVal }}'</pre></p>
+  </div>
+
+
 </template>
