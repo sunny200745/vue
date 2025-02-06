f868022c1178083e8acaa32fbc30f217478b7241
Sunny Sls <sunny@backbase.com>
2025-02-05 16:48:48 -0800

    Set up : Added mock server with api-json

---
+++ src/components/dashboard/Dashboard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    import DataService from '../../services/data';
+    DataService.getUsers((val:any)=>{
+        console.log('#########',val)
+    },{})
+</script>
+
+<template>
+    <div>
+        <h2>
+            Dashboard
+        </h2>
+    </div>
+</template>
+
+<style>
+
+</style>
