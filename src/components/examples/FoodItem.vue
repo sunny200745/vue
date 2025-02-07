c7225d9623e2917cf0657e5a5d6befded7ac7149
Sunny Sls <sunny@backbase.com>
2025-02-07 10:50:41 -0800

    PC comunication

---
+++ src/components/examples/FoodItem.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+import {ref} from 'vue'
+import { computed } from "vue";
+
+
+const props = defineProps(['foodItem'])
+const item = ref(props.foodItem)
+const toggleFavorite = () =>{
+    item.value.favorite = !item.value.favorite
+}
+</script>
+
+<template>
+    <div class="px-2">
+            
+            <div v-on:click="toggleFavorite(item)" class="  h-[300px] w-[220px] mb-2 mt-3 mx-auto bg-slate-100">
+                <div class="p-3">
+                    <h2>{{ item.name }}
+                        <img v-if="item.favorite" width="30px" class="float-right" :src="`src/assets/img_quality.svg`" >
+                    </h2>
+                    <img width="100px" :src="`src/assets/${item.img}`" >
+                    <p>{{ item.desc }}</p>
+                    <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Favourite</button>
+                </div>
+            </div>
+    </div>        
+    
+</template>
+
+<style>
+
+</style>
